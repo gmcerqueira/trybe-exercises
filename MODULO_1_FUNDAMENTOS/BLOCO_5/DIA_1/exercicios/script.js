f2f158924ae2363af6f5c65f38b801f5e4644493
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-03 17:00:21 -0300

    Criando funçao do exercicio 2 - changeColorTrybe()

--- MODULO_1_FUNDAMENTOS/BLOCO_5/DIA_1/exercicios/script.js
+++ MODULO_1_FUNDAMENTOS/BLOCO_5/DIA_1/exercicios/script.js
@@ -4,19 +4,42 @@
   - document.getElementsByClassName()
   - document.getElementsByTagName()
   1 - Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+*/
+function textYouTwoYearsFromNow(text) {
+  let tagsP = document.getElementsByTagName("p");
+
+  let firstP = tagsP[0];
+  firstP.innerText = text;
+}
+
+/*
   2 - Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+*/
+function changeColorTrybe(element) {
+  let mainContent = document.getElementsByClassName(element);
+  console.log(mainContent[0]);
+  mainContent[0].style.backgroundColor = "rgb(76,164,109)";
+}
+
+/*
   3 - Crie uma função que mude a cor do quadrado vermelho para branco.
-  4 - Crie uma função que corrija o texto da tag <h1>.
-  6 - Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+*/
+
+/*
+  4 - Crie uma função que corrija o texto da tag<h1>.
+*/
+
+/*
+6 - Crie uma função que modifique todo o texto da tag < p > para maiúsculo.
+*/
+
+/*
   7 - Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
 
-function textYouTwoYearsFromNow(text) {
-  let tagsP = document.getElementsByTagName('p')
-
-  let firstP = tagsP[0]
-  firstP.innerText = text  
-}
-
 // Execução das funções
-textYouTwoYearsFromNow("Em dois anos me vejo... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur optio libero molestias repellendus minus facilis ipsum doloremque facere vel, architecto numquam distinctio dignissimos dolorem amet id repudiandae totam nesciunt debitis!")
+textYouTwoYearsFromNow(
+  "Em dois anos me vejo... Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur optio libero molestias repellendus minus facilis ipsum doloremque facere vel, architecto numquam distinctio dignissimos dolorem amet id repudiandae totam nesciunt debitis!"
+);
+
+changeColorTrybe("main-content");
