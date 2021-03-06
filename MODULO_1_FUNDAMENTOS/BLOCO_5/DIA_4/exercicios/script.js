ac859dcb72db41431e00ccb6ef8b34276316619b
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-06 20:30:33 -0300

    Melhorias na usabilidade da aplicaçao

--- MODULO_1_FUNDAMENTOS/BLOCO_5/DIA_4/exercicios/script.js
+++ MODULO_1_FUNDAMENTOS/BLOCO_5/DIA_4/exercicios/script.js
@@ -9,16 +9,102 @@
 // Tipo da fonte ( Font family ).
 // Essas preferências devem ser salvas de forma que, ao retornar à página, as preferências que foram previamente configuradas possam ser aplicadas na tela.
 
-function loadPreferences(storage) {
+function verifyStorage() {
+  if (!storage.length) {
+    preferences = {
+      backgroundColor: "#ffffff",
+      color: "#000000",
+      fontSize: "16px",
+      lineHeight: "18px",
+      fontFamily: "Times",
+    };
+    storage.setItem("preferences", JSON.stringify(preferences));
+  }
+}
+
+function loadPreferences() {
   let preferencesArray = [];
-  for (let key in JSON.parse(storage.getItem("preferences"))) {
+  for (const key in JSON.parse(storage.getItem("preferences"))) {
     preferencesArray.push(JSON.parse(storage.getItem("preferences"))[key]);
   }
-  textStyle.backgroundColor = preferencesArray[0];
-  textStyle.color = preferencesArray[1];
-  textStyle.fontSize = preferencesArray[2];
-  textStyle.lineHeight = preferencesArray[3];
-  textStyle.fontFamily = preferencesArray[4];
+  const backgroundColor = preferencesArray[0];
+  const color = preferencesArray[1];
+  const fontSize = preferencesArray[2];
+  const lineHeight = preferencesArray[3];
+  const fontFamily = preferencesArray[4];
+  textStyle.backgroundColor = backgroundColor;
+  textStyle.color = color;
+  textStyle.fontSize = fontSize;
+  textStyle.lineHeight = lineHeight;
+  textStyle.fontFamily = fontFamily;
+  
+  if (preferencesArray) {
+    document.getElementById("background_color").value = backgroundColor;
+    document.getElementById("color").value = color;
+    document.getElementById("font_size").value = parseInt(fontSize);
+    document.getElementById("line_height").value = parseInt(lineHeight);
+    document.getElementById("font_family").value = fontFamily;
+  } else {
+    defaultPreferences();
+  }
+}
+
+function savePreferences() {
+  buttonSavePreferences.addEventListener("click", (event) => {
+    event.preventDefault();
+    let preferences = {
+      backgroundColor: inputs[0].value,
+      color: inputs[1].value,
+      fontSize: inputs[2].value + "px",
+      lineHeight: inputs[3].value + "px",
+      fontFamily: inputs[4].value,
+    };
+    storage.setItem("preferences", JSON.stringify(preferences));
+    document.location.reload();
+  });
+}
+
+function defaultPreferences() {
+  preferences["backgroundColor"] = "#ffffff";
+  preferences["color"] = "#000000";
+  preferences["fontSize"] = "16px";
+  preferences["lineHeight"] = "18px";
+  preferences["fontFamily"] = "Times";
+}
+
+function deletePreferences() {
+  buttonDeletePreferences.addEventListener("click", (event) => {
+    event.preventDefault();
+    defaultPreferences();
+    storage.setItem("preferences", JSON.stringify(preferences));
+    document.location.reload();
+  });
+}
+
+function creatFontOptions() {
+  let fontFamilies = [
+    "sans-serif",
+    "serif",
+    "monospace",
+    "Courier",
+    "Arial",
+    "Caveat",
+    "Cinzel",
+    "Dancing Script",
+    "Grenze Gotisch",
+    "Orbitron",
+    "Roboto",
+    "Times",
+  ];
+  let fontSelect = document.getElementById("font_family");
+
+  for (const font in fontFamilies.sort()) {
+    let option = document.createElement("option");
+    option.style.fontFamily = fontFamilies[font];
+    option.style.fontSize = "20px";
+    option.innerText = fontFamilies[font];
+    fontSelect.appendChild(option);
+  }
 }
 
 function inputChange(input) {
@@ -33,23 +119,15 @@ function inputChange(input) {
 }
 
 let textStyle = document.getElementById("text").style;
-let buttonTextPreferences = document.getElementById("text_preferences");
-let inputs = document.querySelectorAll("input");
+let buttonSavePreferences = document.getElementById("save_preferences");
+let buttonDeletePreferences = document.getElementById("delete_preferences");
+let inputs = document.querySelectorAll(".input");
 let storage = localStorage;
+let preferences = {};
 
+verifyStorage();
+creatFontOptions();
 inputs.forEach((input) => inputChange(input));
-
-loadPreferences(storage);
-
-buttonTextPreferences.addEventListener("click", (event) => {
-  event.preventDefault();
-  let preferences = {
-    backgroundColor: inputs[0].value,
-    color: inputs[1].value,
-    fontSize: inputs[2].value + "px",
-    lineHeight: inputs[3].value + "px",
-    fontFamily: inputs[4].value,
-  };
-  storage.setItem("preferences", JSON.stringify(preferences));
-  document.location.reload();
-});
+loadPreferences();
+deletePreferences();
+savePreferences();
